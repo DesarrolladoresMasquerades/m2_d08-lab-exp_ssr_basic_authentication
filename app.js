33d4baf74021f1ff65eb59d7709f9667e4a5eb88
Marco Santonastasi <marco.santonastasi@gmail.com>
2022-01-02 16:01:32 +0100

    fix project name in homepage

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 require("./config")(app);
 
 // default value for title local
-const projectName = "lab-express-basic-auth";
+const projectName = "exp-ssr-basic-auth";
 const capitalized = (string) =>
   string[0].toUpperCase() + string.slice(1).toLowerCase();
 
